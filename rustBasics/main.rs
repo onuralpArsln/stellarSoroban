6023dcaaf054d6bdbe026e17d9d571d354664c91
onuralpArsln <onuralparslan97@gmail>
2024-10-04 00:19:18 +0300

    readme update

--- rustBasics/main.rs
+++ rustBasics/main.rs
@@ -17,8 +17,32 @@ fn main(){
     arr[0] = 10;
     println!("Updated array: {:?}", arr);
 
+    // to make a for llop on array make array an iterable 
+    for element in arr.iter(){
+    println!("the val is ",element);
+    }
+
+    // also there is a special loop that goes forever it is a glorifeid while true
+        loop {
+        println!("Count is: {}", count);
+        count += 1;
+        if count == 5 {
+            break;
+        }
+
+        // you dont use () in if 
+    if number < 5 {
+        println!("Condition is true");
+    } else {
+        println!("Condition is false");
+    }
 
 
+//  single line if usage 
+
+    let number = 3;
+    let result = if number % 2 == 0 { "even" } else { "odd" };
+    println!("The number is {}", result);
 }
 
 // return type ok ile sonda gösterilir pythonda yapabildiğin gibi
