713b2a0c11f77c9371d2e40e86acfc534c70f4c8
onuralpArsln <onuralparslan97@gmail>
2024-10-05 11:42:52 +0300

    rustrover added and getting used to cargo system

---
+++ cargoTime/first_cargo/target/debug/deps/first_cargo-39b53dd3278d55cb.d
@@ -0,0 +1,5 @@
+/home/parancey/projects/stellarSoroban/cargoTime/first_cargo/target/debug/deps/first_cargo-39b53dd3278d55cb: src/main.rs
+
+/home/parancey/projects/stellarSoroban/cargoTime/first_cargo/target/debug/deps/first_cargo-39b53dd3278d55cb.d: src/main.rs
+
+src/main.rs:
---
+++ cargoTime/first_cargo/target/debug/first_cargo.d
@@ -0,0 +1,1 @@
+/home/parancey/projects/stellarSoroban/cargoTime/first_cargo/target/debug/first_cargo: /home/parancey/projects/stellarSoroban/cargoTime/first_cargo/src/main.rs
